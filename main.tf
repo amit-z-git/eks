725120aa77eb894ef885bbeed7aaf28966073b45
amitzrepo <mr.amitkumarpatel@gmail.com>
2024-11-13 18:38:44 +0530

    latest versions updated

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ locals {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 4.0"
+  version = "5.15.0"
 
   name = local.name
   cidr = local.vpc_cidr
@@ -50,7 +50,7 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.1"
+  version = "20.29.0"
 
   cluster_name                   = local.name
   cluster_endpoint_public_access = true
