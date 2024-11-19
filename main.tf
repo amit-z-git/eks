bfbdd81d9a90c3019ad7fc55dcfe3f6becbbf23d
amitzrepo <mr.amitkumarpatel@gmail.com>
2024-11-19 19:26:31 +0530

    unable to access from local

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ terraform {
 data "aws_availability_zones" "available" {}
 
 locals {
-  region          = "ap-south-2"
-  name            = "amit-eks-cluster"
-  vpc_cidr        = "10.123.0.0/16"
-  azs             = slice(data.aws_availability_zones.available.names, 0, 3)
+  region   = "ap-south-2"
+  name     = "amit-eks-cluster"
+  vpc_cidr = "10.123.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
   tags = {
     Name = local.name
   }
@@ -37,81 +37,90 @@ module "vpc" {
   intra_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 52)]
 
   enable_nat_gateway = true
-  enable_vpn_gateway = true
-
-  tags = {
-    "kubernetes.io/cluster/${local.name}" = "shared"
-  }
 
   public_subnet_tags = {
-    "kubernetes.io/cluster/${local.name}" = "shared"
-    "kubernetes.io/role/elb"              = "1"
+    "kubernetes.io/role/elb" = 1
   }
 
   private_subnet_tags = {
-    "kubernetes.io/cluster/${local.name}" = "shared"
-    "kubernetes.io/role/internal-elb"     = "1"
+    "kubernetes.io/role/internal-elb" = 1
   }
 }
 
+# module "eks" {
+#   source          = "terraform-aws-modules/eks/aws"
+#   version         = "20.29.0"
+#   cluster_name    = local.name
+#   cluster_version = "1.31"
+#   subnet_ids      = module.vpc.private_subnets
+
+#   enable_irsa = true
+
+#   tags = {
+#     cluster = "dev"
+#   }
+
+#   vpc_id = module.vpc.vpc_id
+
+#   eks_managed_node_group_defaults = {
+#     ami_type               = "AL2_x86_64"
+#     instance_types         = ["t3.medium"]
+#     #vpc_security_group_ids = [aws_security_group.all_worker_mgmt.id]
+#   }
+
+#   eks_managed_node_groups = {
+
+#     node_group = {
+#       min_size     = 1
+#       max_size     = 5
+#       desired_size = 1
+#     }
+#   }
+# }
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "20.29.0"
-  cluster_name    = local.name
-  cluster_version = "1.31"
-  subnet_ids      = module.vpc.private_subnets
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.29.0"
 
-  enable_irsa = true
+  cluster_name                   = local.name
+  cluster_endpoint_public_access = true
 
-  tags = {
-    cluster = "dev"
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
   }
 
-  vpc_id = module.vpc.vpc_id
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.intra_subnets
 
+  # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
-    ami_type               = "AL2_x86_64"
-    instance_types         = ["t3.medium"]
-    vpc_security_group_ids = [aws_security_group.all_worker_mgmt.id]
+    ami_type       = "AL2_x86_64"
+    instance_types = ["t3.medium"]
+
+    attach_cluster_primary_security_group = true
   }
 
   eks_managed_node_groups = {
-
-    node_group = {
+    amc-cluster-wg = {
       min_size     = 1
-      max_size     = 5
+      max_size     = 3
       desired_size = 1
+
+      tags = {
+        ExtraTag = "dev"
+      }
     }
   }
-}
 
-resource "aws_security_group" "all_worker_mgmt" {
-  name_prefix = "all_worker_management"
-  vpc_id      = module.vpc.vpc_id
-}
-
-resource "aws_security_group_rule" "all_worker_mgmt_ingress" {
-  description       = "allow inbound traffic from eks"
-  from_port         = 0
-  protocol          = "-1"
-  to_port           = 0
-  security_group_id = aws_security_group.all_worker_mgmt.id
-  type              = "ingress"
-  cidr_blocks = [
-    "10.0.0.0/8",
-    "172.16.0.0/12",
-    "192.168.0.0/16",
-  ]
-}
-
-resource "aws_security_group_rule" "all_worker_mgmt_egress" {
-  description       = "allow outbound traffic to anywhere"
-  from_port         = 0
-  protocol          = "-1"
-  security_group_id = aws_security_group.all_worker_mgmt.id
-  to_port           = 0
-  type              = "egress"
-  cidr_blocks       = ["0.0.0.0/0"]
+  tags = local.tags
 }
 
 output "update_kubeconfig_command" {
