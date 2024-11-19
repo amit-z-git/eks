b27e99e74e77daed333f7ff19da66319df6b01b5
amitzrepo <mr.amitkumarpatel@gmail.com>
2024-11-19 13:12:09 +0530

    work in progress

--- main.tf
+++ main.tf
@@ -12,22 +12,24 @@ terraform {
   }
 }
 
+data "aws_availability_zones" "available" {}
+
 locals {
   region          = "ap-south-2"
   name            = "amit-eks-cluster"
   vpc_cidr        = "10.123.0.0/16"
-  azs             = ["ap-south-2a", "ap-south-2b"]
-  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
-  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
-  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
+  azs             = data.aws_availability_zones.available.names
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.10.0/24", "10.0.20.0/24", "10.0.30.0/24"]
+  intra_subnets   = ["10.0.101.0/24", "10.0.201.0/24", "10.0.301.0/24"]
   tags = {
-    Example = local.name
+    Name = local.name
   }
 }
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.15.0"
+  version = "5.16.0"
 
   name = local.name
   cidr = local.vpc_cidr
@@ -35,64 +37,85 @@ module "vpc" {
   azs             = local.azs
   private_subnets = local.private_subnets
   public_subnets  = local.public_subnets
-  intra_subnets   = local.intra_subnets
 
   enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    "kubernetes.io/cluster/${local.name}" = "shared"
+  }
 
   public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
+    "kubernetes.io/cluster/${local.name}" = "shared"
+    "kubernetes.io/role/elb"              = "1"
   }
 
   private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
+    "kubernetes.io/cluster/${local.name}" = "shared"
+    "kubernetes.io/role/internal-elb"     = "1"
   }
 }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.29.0"
+  version = "~> 20.29.0"
 
-  cluster_name                   = local.name
+  cluster_name    = "my-cluster"
+  cluster_version = "1.31"
+
   cluster_endpoint_public_access = true
 
   cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
+    coredns                = {}
+    eks-pod-identity-agent = {}
+    kube-proxy             = {}
+    vpc-cni                = {}
   }
 
-  vpc_id                   = module.vpc.vpc_id
+  vpc_id                   = module.vpc
   subnet_ids               = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.intra_subnets
+  control_plane_subnet_ids = module.vpc.public_subnets
 
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
-    ami_type       = "AL2_x86_64"
-    instance_types = ["m5.large"]
-
-    attach_cluster_primary_security_group = true
+    instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
   }
 
   eks_managed_node_groups = {
-    amc-cluster-wg = {
+    example = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["m5.xlarge"]
+
       min_size     = 1
-      max_size     = 2
+      max_size     = 5
       desired_size = 1
+    }
+  }
 
-      instance_types = ["t3.medium"]
-      capacity_type  = "SPOT"
+  # Cluster access entry
+  # To add the current caller identity as an administrator
+  enable_cluster_creator_admin_permissions = true
 
-      tags = {
-        ExtraTag = "amit-eks"
+  access_entries = {
+    # One access entry with a policy associated
+    example = {
+      principal_arn = "arn:aws:iam::123456789012:role/something"
+
+      policy_associations = {
+        example = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
+          access_scope = {
+            namespaces = ["default"]
+            type       = "namespace"
+          }
+        }
       }
     }
   }
 
-  tags = local.tags
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
 }
