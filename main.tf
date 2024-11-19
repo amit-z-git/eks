3ea426a0be592f874b2d4a7313eab999799267f1
Amit <mr.amitkumarpatel@gmail.com>
2024-11-19 14:38:46 +0530

    file updated

--- main.tf
+++ main.tf
@@ -80,77 +80,12 @@ module "eks" {
   eks_managed_node_groups = {
 
     node_group = {
-      min_size     = 2
-      max_size     = 6
-      desired_size = 2
-    }
-  }
-}
-
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.29.0"
-
-  cluster_name    = "my-cluster"
-  cluster_version = "1.31"
-
-  cluster_endpoint_public_access = true
-
-  cluster_addons = {
-    coredns                = {}
-    eks-pod-identity-agent = {}
-    kube-proxy             = {}
-    vpc-cni                = {}
-  }
-
-  vpc_id                   = module.vpc
-  subnet_ids               = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.public_subnets
-
-  # EKS Managed Node Group(s)
-  eks_managed_node_group_defaults = {
-    instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
-  }
-
-  eks_managed_node_groups = {
-    example = {
-      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["m5.xlarge"]
-
       min_size     = 1
       max_size     = 5
       desired_size = 1
     }
   }
-
-  # Cluster access entry
-  # To add the current caller identity as an administrator
-  enable_cluster_creator_admin_permissions = true
-
-  access_entries = {
-    # One access entry with a policy associated
-    example = {
-      principal_arn = "arn:aws:iam::123456789012:role/something"
-
-      policy_associations = {
-        example = {
-          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
-          access_scope = {
-            namespaces = ["default"]
-            type       = "namespace"
-          }
-        }
-      }
-    }
-  }
-
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
-  }
 }
-
 
 resource "aws_security_group" "all_worker_mgmt" {
   name_prefix = "all_worker_management"
