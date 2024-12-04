ee8ece6dce707c5e42eb2f8ccb34af9d1dfd306c
amitzrepo <mr.amitkumarpatel@gmail.com>
2024-12-04 15:02:01 +0530

    change in eks

---
+++ eks.tf
@@ -0,0 +1,63 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = local.name
+  cluster_version = "1.31"
+
+  cluster_endpoint_public_access = true
+
+  cluster_addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {}
+    kube-proxy             = {}
+    vpc-cni                = {}
+  }
+
+  vpc_id                   = module.vpc.default_vpc_id
+  subnet_ids               = module.vpc.public_subnets
+  control_plane_subnet_ids = module.vpc.intra_subnets
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    instance_types = ["t3.medium"]
+  }
+
+  eks_managed_node_groups = {
+    example = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["t3.medium"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 1
+    }
+  }
+
+  # Cluster access entry
+  # To add the current caller identity as an administrator
+  enable_cluster_creator_admin_permissions = true
+
+  access_entries = {
+    # One access entry with a policy associated
+    example = {
+      principal_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+
+      policy_associations = {
+        example = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
+          access_scope = {
+            namespaces = ["default"]
+            type       = "namespace"
+          }
+        }
+      }
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
--- main.tf
+++ main.tf
@@ -12,13 +12,20 @@ terraform {
   }
 }
 
-data "aws_availability_zones" "available" {}
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-2"
+}
+
+data "aws_availability_zones" "available" {
+  #region = local.region
+}
 
 locals {
   region   = "ap-south-2"
   name     = "amit-eks-cluster"
   vpc_cidr = "10.123.0.0/16"
-  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+  azs      = slice(data.aws_availability_zones.available.names, 0, length(data.aws_availability_zones.available.names))
   tags = {
     Name = local.name
   }
@@ -32,9 +39,9 @@ module "vpc" {
   cidr = local.vpc_cidr
 
   azs             = local.azs
-  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
-  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
-  intra_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 52)]
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 1)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 4)]
+  intra_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 7)]
 
   enable_nat_gateway = true
 
@@ -47,82 +54,7 @@ module "vpc" {
   }
 }
 
-# module "eks" {
-#   source          = "terraform-aws-modules/eks/aws"
-#   version         = "20.29.0"
-#   cluster_name    = local.name
-#   cluster_version = "1.31"
-#   subnet_ids      = module.vpc.private_subnets
-
-#   enable_irsa = true
-
-#   tags = {
-#     cluster = "dev"
-#   }
-
-#   vpc_id = module.vpc.vpc_id
-
-#   eks_managed_node_group_defaults = {
-#     ami_type               = "AL2_x86_64"
-#     instance_types         = ["t3.medium"]
-#     #vpc_security_group_ids = [aws_security_group.all_worker_mgmt.id]
-#   }
-
-#   eks_managed_node_groups = {
-
-#     node_group = {
-#       min_size     = 1
-#       max_size     = 5
-#       desired_size = 1
-#     }
-#   }
-# }
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "20.29.0"
-
-  cluster_name                   = local.name
-  cluster_endpoint_public_access = true
-
-  cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-  }
-
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.intra_subnets
-
-  # EKS Managed Node Group(s)
-  eks_managed_node_group_defaults = {
-    ami_type       = "AL2_x86_64"
-    instance_types = ["t3.medium"]
-
-    attach_cluster_primary_security_group = true
-  }
-
-  eks_managed_node_groups = {
-    amc-cluster-wg = {
-      min_size     = 1
-      max_size     = 3
-      desired_size = 1
-
-      tags = {
-        ExtraTag = "dev"
-      }
-    }
-  }
-
-  tags = local.tags
-}
-
 output "update_kubeconfig_command" {
-  value = format("%s %s %s %s", "aws eks update-kubeconfig --region", local.region, "--name", module.eks.cluster_name)
+  value = format("aws eks update-kubeconfig --region %s --name %s", local.region, module.eks.cluster_name)
 }
+
