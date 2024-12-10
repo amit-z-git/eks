d806c892555056b218a7198213619d423bbc3693
amitzrepo <mr.amitkumarpatel@gmail.com>
2024-12-10 20:20:37 +0530

    module and eks_cluster

--- main.tf
+++ main.tf
@@ -43,7 +43,9 @@ module "vpc" {
   public_subnets  = local.public_subnets
   intra_subnets   = local.intra_subnets
 
-  enable_nat_gateway = true
+  enable_nat_gateway   = true
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   public_subnet_tags = {
     "kubernetes.io/role/elb" = 1
